6383201207991a3ae940052a54be5c0fdbac3628
kmfb21 <kmfb21@gmail.com>
2016-04-23 21:30:22 -0400

    UTF-8 encode

--- src/com/bofang/serversocket/ChatSocket.java
+++ src/com/bofang/serversocket/ChatSocket.java
@@ -1,26 +1,24 @@
 package com.bofang.serversocket;
 
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
 import java.net.Socket;
 
 public class ChatSocket extends Thread {
-	
+
 	Socket socket;
-	
+
 	public ChatSocket(Socket s) {
 		this.socket = s;
 	}
-	
+
 	public void out(String out) {
 		try {
-			socket.getOutputStream().write(out.getBytes());
+			socket.getOutputStream().write(out.getBytes("UTF-8"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	@Override
 	public void run() {
 		int count = 0;
